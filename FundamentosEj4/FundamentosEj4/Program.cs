6a0cddceedc4e4f367d2abb40de4f6d24e00fd6b
Ramsés Castro <ramses.castro@lennken.com>
2022-07-19 14:38:46 -0500

    Se termina el ejercicio 4

--- FundamentosEj4/FundamentosEj4/Program.cs
+++ FundamentosEj4/FundamentosEj4/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FundamentosEj4;
+
+string numeroIngresado = "";
+numeroIngresado = numeroIngresado.MensajePedirNumero();
+Console.WriteLine($"¿El valor {numeroIngresado.ConvertirStringNumero()} es par? {numeroIngresado.ConvertirStringNumero().EsPar()}");
+Console.WriteLine("Hola Mundo! este es un conteo de espacio, el resultado debería ser de: 13".ContarCaracter());
+Console.WriteLine($"Palabra con vocales: Murcielago, palabra sin vocales: {"Murcielago".RemoverVocales()}");
---
+++ FundamentosEj4/FundamentosEj4/Utility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FundamentosEj4
+{
+    public static class Utility
+    {
+        public static bool EsPar(this int valor) {
+            return valor % 2 == 0;
+        }
+
+        public static bool Validacion(this string numeroIngresado) {
+            Match resultMatch = Regex.Match(numeroIngresado, @"^\d+$");
+            return resultMatch.Success;
+        }
+
+        public static int ConvertirStringNumero(this string numeroIngresado) {
+            int valor = int.Parse(numeroIngresado);
+            return valor;
+        }
+
+        public static int ContarCaracter(this string cadena) {
+            int cuenta = 0;
+            foreach(char letra in cadena){
+                if (Char.IsWhiteSpace(letra)) {
+                    cuenta += 1;
+                }
+            }
+            return cuenta;
+        }
+
+        public static string RemoverVocales(this string cadena) {
+            string fraseCambiada = Regex.Replace(cadena, "[aeiou]", "");
+            return fraseCambiada;
+        }
+
+        public static string MensajePedirNumero(this string cadena) {
+            while (true)
+            {
+                Console.Write("Ingresa un número: ");
+                cadena = Console.ReadLine();
+
+                if (!cadena.Validacion())
+                    Console.WriteLine("Debe de ser un número válido.");
+                else
+                    break;
+            }
+            return cadena;
+        }
+
+
+
+        
+
+    }
+}
