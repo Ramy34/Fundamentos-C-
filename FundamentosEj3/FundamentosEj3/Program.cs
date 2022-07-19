27156f7a246357a4af5ece9a454916f7f78c882a
Ramsés Castro <ramses.castro@lennken.com>
2022-07-19 13:23:05 -0500

    Se agregan nuevos archivos

---
+++ FundamentosEj3/FundamentosEj3/Program.cs
@@ -0,0 +1,5 @@
+﻿using FundamentosEj3;
+
+string nombre = "Ramsés";
+Console.WriteLine(nombre.SayHi());
+Console.ReadLine();
---
+++ FundamentosEj3/FundamentosEj3/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosEj3
+{
+    public static class Utility
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        public static string SayHi(this string valor)
+        {
+            return $"Hola {valor}";
+        
+        }
+
+
+    }
+}
